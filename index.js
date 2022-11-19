7d825a65ffc259afc4031753e6a14220d71b2817
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-19 13:52:30 +0300

    commit tree e30334d624b619770372efa5d742de0b7db0d41c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cards = (["moses", "remmy", "king"], "birthday");
+
+function writeCards(name, event) {
+    let thankYouCards = []
+    for (let i = 0; i < name.length; i++) {
+        thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+
+    }
+    return thankYouCards
+}
+writeCards(["moses", "remmy", "king"], "birthday");
+
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number--;
+    }
+    console.log(number);
+}
